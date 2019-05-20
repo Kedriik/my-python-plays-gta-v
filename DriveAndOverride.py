b4cf1273a7a938c527a88992604c67161f2233c5
Kedrowsky <kedro123@gmail.com>
2019-05-20 09:33:37 +0200

    Agent in progress

--- DriveAndOverride.py
+++ DriveAndOverride.py
@@ -7,8 +7,10 @@ from directkeys import PressKey, ReleaseKey, W,A,S,D
 from random import random
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-from playground import agentZ
+from playground import agentZ, agentY
 
+capture_region = (0,40,800,640)
+reshape_size = (300,400)
 def keysToCategories(keys):
     if keys == [0,0,0,0]: #roll
         return [1,0,0,0,0,0,0,0,0]
@@ -50,7 +52,7 @@ def categoriesToKeys(categories):
     if categories == [0,0,0,0,0,0,0,0,1]: #brake
         return [0,0,0,1]
 
-    print("C to K: WARNING NON FOUND!")
+    #print("C to K: WARNING NON FOUND!")
         
 def keys_to_output(keys):
     #[A,W,D,S]
@@ -64,27 +66,29 @@ def keys_to_output(keys):
     if 'S' in keys:
         output[3] = 1        
     return output
+
 def update_pressed_keys(keys):
     ReleaseKey(W)
     ReleaseKey(S)
     ReleaseKey(A)
     ReleaseKey(D)
     if keys[0] == 1:
-        PressKey(A)
-    if keys[1] == 1:
         PressKey(W)
-    if keys[2] == 1:
-        PressKey(D)
-    if keys[3] == 1:
+    if keys[1] == 1:
         PressKey(S)
+    if keys[2] == 1:
+        PressKey(A)
+    if keys[3] == 1:
+        PressKey(D)
         
 def releaseKeys():
     ReleaseKey(W)
     ReleaseKey(S)
     ReleaseKey(A)
     ReleaseKey(D)
+
 gamma = 0.99
-def discount_rewards(r):
+def _discount_rewards(r):
     """ take 1D float array of rewards and compute discounted reward """
     discounted_r = np.zeros_like(r)
     running_add = 0
@@ -93,93 +97,90 @@ def discount_rewards(r):
         discounted_r[t] = running_add
     return discounted_r
 
-def RandomAgent(state, reward):
-    categories =  [0,0,0,0,0,0,0,0,0]
-    for i in range(len(categories)):
-        categories[i] = random()
-    #print(categories)
-    categories = np.array(categories)
-    #print(categories)
-    #print(np.argmax(categories))
-    max_index = np.argmax(categories)
-    for i in range(len(categories)):
-        if i == max_index:
-            categories[i] = 1
-        else:
-            categories[i] = 0
-            
-    #print(categories)
-    keys = categoriesToKeys(categories.tolist())
-    #print(keys)
-    return keys
+def discount_rewards(rewards, discount_rate):
+    discounted_rewards = np.zeros(len(rewards))
+    cumulative_rewards = 0
+    for step in reversed(range(len(rewards))):
+        cumulative_rewards = rewards[step] + cumulative_rewards * discount_rate
+        discounted_rewards[step] = cumulative_rewards
+    return discounted_rewards
 
-class agent():
-    def __init__(self, lr, s_size,a_size,h_size):
-        #These lines established the feed-forward part of the network.
-        #The agent takes a state and produces an action.
-        self.state_in= tf.placeholder(shape=[None,s_size[0],s_size[1],s_size[2]],dtype=tf.float32)
-        print("state in:",self.state_in)
-        conv1 = slim.conv2d(self.state_in,num_outputs = 64, kernel_size = [4,4])
-        print("conv1:",conv1)
-        max_pool1 = slim.max_pool2d(conv1,[2, 2])
-        print("max_pool1:",max_pool1)
-        hidden = slim.fully_connected(max_pool1,h_size,biases_initializer=None,activation_fn=tf.nn.relu)
-        print("hidden:",hidden)
-        self.output = slim.fully_connected(hidden,a_size,activation_fn=tf.nn.softmax,biases_initializer=None)
-        print("output:",self.output)
-        self.chosen_action = tf.argmax(self.output,1)
+def discount_and_normalize_rewards(all_rewards, discount_rate):
+    all_discounted_rewards = [discount_rewards(rewards, discount_rate) for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discounted_rewards - reward_mean)/reward_std for discounted_rewards in all_discounted_rewards]
 
+def get_state():
+    state = grab_screen(region = capture_region)
+    state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+    state = cv2.resize(state,(reshape_size[1],reshape_size[0]))
+    state = state.reshape(reshape_size[0],reshape_size[1],1)
+    return state
+
+def teach_agent(agent, history,sess):
+    history = np.array(history)
+    frames  = np.array(history[:,0])
+    rewards = np.array(discount_rewards(history[:,3],0.99))
+    accs = np.array(history[:,1])
+    steers = (history[:,2])
+    #steers = [np.array[steer] for steer in steers]
+    #=([steer for steer in steers])
+    #print(Y)
+    steers  = np.array([steer for steer in steers]).reshape(-1,3).astype(float)
+    accs    = np.array([acc for acc in accs]).reshape(-1,3).astype(float)
+    frames  = np.array([i for i in frames]).reshape(-1,300,400,1).astype(float)
+    #print(X)
+      
+    ret = sess.run(agent.steer_in, feed_dict={agent.acc_in:accs, agent.steer_in:steers, agent.state_in:frames})
+def m_multinomial(acc, steer):
+    acc_i = np.random.multinomial(1,acc)
+    steer_i = np.random.multinomial(1,steer)
+    return acc_i, steer_i
 def main():        
     collectData = False
     clickFlag = False
-    print("Press Z to start collecting data")
-    reward = 0
-    capture_region = (0,40,800,640)
-    reshape_size = (300,400)
-    last_state = grab_screen(region = capture_region)
-    last_state = cv2.cvtColor(last_state, cv2.COLOR_BGR2GRAY)
-    last_state = cv2.resize(last_state,(reshape_size[1],reshape_size[0]))
-    last_state = last_state.reshape(reshape_size[0],reshape_size[1],1)
-    
+    last_state = get_state()    
     tf.reset_default_graph() #Clear the Tensorflow graph.
     myAgent = agentZ(0.1,(300,400,1),9,11)
     history = []
     delta_time = 0
     state = last_state
     init = tf.global_variables_initializer()
-    reward = 0 #[0,0,0,0,0,0,0,0,0]
+    reward = 0 
 # Launch the tensorflow graph
     with tf.Session() as sess:
         sess.run(init)
         i = 0
-        gradBuffer = sess.run(tf.trainable_variables())
-        for ix,grad in enumerate(gradBuffer):
-            gradBuffer[ix] = grad * 0
         print("Loop starts")
+        print("Press Z to start collecting data")
         while True:
             time0 = time.time()
-            #make action
-            action_keys = sess.run(myAgent.output,feed_dict={myAgent.state_in:[state]})
-            max_id = np.argmax(action_keys)
-            #    update_pressed_keys(action_keys)
-            action = [0,0,0,0,0,0,0,0,0]
-            action[max_id] = 1
-            agent_keys = categoriesToKeys(action)
-# =============================================================================
-#             if(collectData):
-#                 #Agent's decision
-#                 update_pressed_keys(agent_keys)
-# =============================================================================
-            #-----------
+            #accelerate_or_brake
+            acc,steer = sess.run([myAgent.acc_action,myAgent.steer_action],
+                                 feed_dict={myAgent.state_in:[state]})
+            #action = [acc[0][0],brake[0][0],left[0][0],right[0][0]]
+            acc, steer = m_multinomial(acc[0],steer[0])
+            
+            acc = np.array([acc[0],acc[1],acc[2]])
+            steer = np.array([steer[0],steer[1],steer[2]])
+            #print(acc)
+            releaseKeys();
+            #if  collectData:
+            #    print(action[0],action[1])
+                
+                #Agent's decision
+                #update_pressed_keys(action)
+            #-----------xxzzxxzzzzzzxzzzxxzzzzzzxzzzxxzzzzzzz
             if collectData:
-                policy = [1,1,1,1,1,1,1,1,1]
-                reward = reward + delta_time*np.array(policy)
+                reward = reward + delta_time
                 state = grab_screen(region = capture_region)
                 state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
                 state = cv2.resize(state, (reshape_size[1], reshape_size[0]))
                 state = state.reshape(reshape_size[0], reshape_size[1],1)
                 #-----------------------------
-                history.append([last_state,action,reward,state]) 
+                history.append([last_state,acc, steer,reward,state]) 
                 last_state = state        
             keys = []
             keys   = key_check()
@@ -190,21 +191,8 @@ def main():
                     collectData = False
                     releaseKeys()
                     print('On time based reward:',reward)
-                    history = np.array(history)
-                    frames = np.array(history[:,0])
-                    rewards = np.array(discount_rewards(history[:,2]))
-                    actions = np.array(history[:,1])
-# =============================================================================
-#                     i = 0
-#                     for frame in frames:
-#                         print(actions[i])
-#                         i = i + 1
-#                         cv2.imshow('',frame.reshape((reshape_size[0],reshape_size[1],1)))
-#                         if cv2.waitKey(25) & 0xFF==ord('q'):
-#                             cv2.destroyAllWindows()
-#                             break
-#                     cv2.destroyAllWindows()
-# =============================================================================
+                    teach_agent(myAgent, history,sess)                    
+
                     del history
                     history = []
                     #update agent
--- playground.py
+++ playground.py
@@ -8,26 +8,166 @@ import tensorflow as tf
 from tensorflow.contrib import slim
 class agentZ():
     def __init__(self, lr, s_size,a_size,h_size):
+        self.state_in = tf.placeholder(shape=[None]+list(s_size),dtype=tf.float32)
+        conv1         = slim.conv2d(self.state_in,num_outputs = 16, kernel_size = [4,4])
+        max_pool1     = slim.max_pool2d(conv1,[2, 2])
+        hidden        = slim.fully_connected(max_pool1,h_size,biases_initializer=None,activation_fn=tf.nn.relu)
+        self.flatten  = slim.flatten(hidden)
+        self.logits   = slim.fully_connected(self.flatten,num_outputs=a_size,activation_fn=tf.nn.sigmoid,biases_initializer=None)
+        self.action   = tf.multinomial(tf.log(self.output),num_samples = 1)
+
+class agentY():
+    def __init__(self,lr,s_size,a_size,h_size):
+        self.state_in = tf.placeholder(shape = [None]+list(s_size),dtype=tf.float32)
+        conv1         = tf.layers.conv2d(self.state_in,32,4,strides=(4, 4))
+        max_pool1     = tf.layers.max_pooling2d(conv1,32,4)
+        flatten       = tf.layers.flatten(max_pool1)
+        hidden        = tf.layers.dense(flatten,4096,activation=tf.nn.tanh)
+        
+        
+        hidden_acc              = tf.layers.dense(hidden,2048, activation=tf.nn.elu)
+        self.logits_acc         = tf.layers.dense(hidden_acc,1, activation=tf.nn.sigmoid)
+        acc_press_or_not        = tf.concat(axis=1, values=[self.logits_acc, 1 - self.logits_acc])
+        self.acc_action         = tf.multinomial(tf.log(acc_press_or_not), num_samples=1)
+        self.y_acc              = 1. - tf.to_float(self.acc_action)        
+        
+        hidden_brake            = tf.layers.dense(hidden,2048, activation=tf.nn.elu)
+        self.logits_brake       = tf.layers.dense(hidden_brake,1, activation=tf.nn.sigmoid)
+        brake_press_or_not      = tf.concat(axis=1, values=[self.logits_brake, 1 - self.logits_brake])
+        self.brake_action       = tf.multinomial(tf.log(brake_press_or_not), num_samples=1)
+        self.y_brake            = 1. - tf.to_float(self.brake_action)        
+        
+        hidden_steer_left       = tf.layers.dense(hidden,2048, activation=tf.nn.elu)
+        self.logits_left        = tf.layers.dense(hidden_steer_left,1, activation=tf.nn.sigmoid)
+        left_press_or_not       = tf.concat(axis=1, values=[self.logits_left, 1 - self.logits_left])
+        self.left_action        = tf.multinomial(tf.log(left_press_or_not), num_samples=1)
+        self.y_left             = 1. - tf.to_float(self.left_action)        
+        
+        hidden_steer_right      = tf.layers.dense(hidden,2048, activation=tf.nn.elu)
+        self.logits_right       = tf.layers.dense(hidden_steer_right,1, activation=tf.nn.sigmoid)
+        right_press_or_not      = tf.concat(axis=1, values=[self.logits_right, 1 - self.logits_right])
+        self.right_action       = tf.multinomial(tf.log(right_press_or_not), num_samples=1)
+        self.y_right            = 1. - tf.to_float(self.right_action)        
+        
+
+class agentZ():
+    def __init__(self,lr,s_size,a_size,h_size):
+        self.state_in = tf.placeholder(shape = [None]+list(s_size),dtype=tf.float32)
+        conv1         = tf.layers.conv2d(self.state_in,32,4,strides=(4, 4))
+        max_pool1     = tf.layers.max_pooling2d(conv1,32,4)
+        flatten       = tf.layers.flatten(max_pool1)
+        hidden        = tf.layers.dense(flatten,4096,activation=tf.nn.tanh)
+        
+        
+        hidden_acc            = tf.layers.dense(hidden,2048, activation=tf.nn.relu)
+        self.acc_action       = tf.layers.dense(hidden_acc,3, activation=tf.nn.softmax)
+        #self.acc_output       = tf.to_float(tf.equal(tf.reduce_max(self.dense_acc_output, axis=1), self.dense_acc_output))
+        #self.acc_logits       = tf.argmax(self.acc_output, axis=1)
+        #self.acc_action       = tf.multinomial(tf.log(self.dense_acc_output),1)
+        
+        hidden_steer          = tf.layers.dense(hidden,2048, activation=tf.nn.relu)
+        self.steer_action     = tf.layers.dense(hidden_steer,3, activation=tf.nn.softmax)
+        #self.steer_output       = tf.to_float(tf.equal(tf.reduce_max(self.dense_steer_output, axis=1), self.dense_steer_output))
+        #self.logits             = tf.argmax(self.steer_output, axis=1)
+        #self.steer_action       = tf.multinomial(tf.log(self.dense_steer_output),1)
+        self.acc_in           = tf.placeholder(shape =[None,3],dtype=tf.float32) 
+        self.steer_in         = tf.placeholder(shape =[None,3],dtype=tf.float32) 
+        
+        cross_entropy_acc     = tf.nn.softmax_cross_entropy_with_logits(labels=self.acc_in, logits=self.acc_action)
+        cross_entropy_steer   = tf.nn.softmax_cross_entropy_with_logits(labels=self.steer_in, logits=self.steer_action)
+        optimizer             = tf.train.AdamOptimizer(lr)
+
+        grads_and_vars = optimizer.compute_gradients(cross_entropy_acc)
+        print(grads_and_vars)
+        gradients = [grad for grad, variable in grads_and_vars]
+        gradient_placeholders = []
+        grads_and_vars_feed = []
+        for grad, variable in grads_and_vars:
+            gradient_placeholder = tf.placeholder(tf.float32, shape=grad.get_shape())
+            gradient_placeholders.append(gradient_placeholder)
+            grads_and_vars_feed.append((gradient_placeholder, variable))
+        training_op = optimizer.apply_gradients(grads_and_vars_feed)
+        
+
+        
+tf.reset_default_graph()
+testAgent = agentZ(0.1,(300,400,1),9,11)  
+     
+class lesson2agent():
+    def __init__(self, lr, s_size,a_size,h_size):
         #These lines established the feed-forward part of the network.
         #The agent takes a state and produces an action.
+        self.state_in= tf.placeholder(shape=[None,s_size],dtype=tf.float32)
+        hidden = slim.fully_connected(self.state_in,h_size,biases_initializer=None,activation_fn=tf.nn.relu)
+        self.output = slim.fully_connected(hidden,a_size,activation_fn=tf.nn.softmax,biases_initializer=None)
+        #print("Output:",self.output)
+        self.chosen_action = tf.argmax(self.output,1)
+
+        #The next six lines establish the training proceedure.
+        #We feed the reward and chosen action into the network
+        #to compute the loss, and use it to update the network.
+        self.reward_holder = tf.placeholder(shape=[None],dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[None],dtype=tf.int32)
+        
+        self.indexes = tf.range(0, tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
+        #print("output[0]:",self.output[0])
+        #print("output[1]:",self.output[1])
+        #print("action_holder:",self.action_holder)
+        #print("indexes:",self.indexes)
+        self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes)
+        #print("responsible outputs:",self.responsible_outputs)
+        #print("reward holder:",self.reward_holder)
+        self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs)*self.reward_holder)
+        
+        tvars = tf.trainable_variables()
+        self.gradient_holders = []
+        for idx,var in enumerate(tvars):
+            placeholder = tf.placeholder(tf.float32,name=str(idx)+'_holder')
+            self.gradient_holders.append(placeholder)
+        
+        self.gradients = tf.gradients(self.loss,tvars)
+        
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+        self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders,tvars))
+myAgent = lesson2agent(lr=1e-2,s_size=4,a_size=2,h_size=8) #Load the agent.
+
+class lesson2agent2():
+    def __init__(self, lr, s_size,a_size,h_size):
         self.state_in= tf.placeholder(shape=[None]+list(s_size),dtype=tf.float32)
-        print("state in:",self.state_in)
-        conv1 = slim.conv2d(self.state_in,num_outputs = 64, kernel_size = [4,4])
-        #print("conv1:",conv1)
+        conv1 = slim.conv2d(self.state_in,num_outputs = 16, kernel_size = [4,4])
         max_pool1 = slim.max_pool2d(conv1,[2, 2])
-        #print("max_pool1:",max_pool1)
         hidden = slim.fully_connected(max_pool1,h_size,biases_initializer=None,activation_fn=tf.nn.relu)
         flatten = slim.flatten(hidden)
-        print("flatten:",flatten)
-        #print("hidden:",hidden)
         self.output = slim.fully_connected(flatten,num_outputs=a_size,activation_fn=tf.nn.softmax,biases_initializer=None)
-        #print("a size:", a_size)
-        #print("output:",self.output)
-        #self.chosen_action = tf.argmax(self.output,1)
+        #print("Output:",self.output)
+        self.chosen_action = tf.argmax(self.output,1)
 
-#testAgent = agent(0.1,(300,400,1),9,11)
-
-
+        #The next six lines establish the training proceedure.
+        #We feed the reward and chosen action into the network
+        #to compute the loss, and use it to update the network.
+        self.reward_holder = tf.placeholder(shape=[None],dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[None],dtype=tf.int32)
+        
+        self.indexes = tf.range(0, tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
+        #print("output[0]:",self.output[0])
+        #print("output[1]:",self.output[1])
+        #print("action_holder:",self.action_holder)
+        #print("indexes:",self.indexes)
+        self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes)
+        #print("responsible outputs:",self.responsible_outputs)
+        #print("reward holder:",self.reward_holder)
+        self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs)*self.reward_holder)
+        
+        tvars = tf.trainable_variables()
+        self.gradient_holders = []
+        for idx,var in enumerate(tvars):
+            placeholder = tf.placeholder(tf.float32,name=str(idx)+'_holder')
+            self.gradient_holders.append(placeholder)
+        
+        self.gradients = tf.gradients(self.loss,tvars)
+        
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+        self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders,tvars))
 # =============================================================================
 # def keysToCategories(keys):
 #     if keys == [0,0,0,0]: #roll
